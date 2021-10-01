ff66867c53b20dcec886400ba32074a26d4c5214
Adrian Carmona Lupiáñez <43786395+adriancarmona999@users.noreply.github.com>
2021-10-01 10:01:37 +0200

    empesamo

---
+++ P1.java
@@ -0,0 +1,14 @@
+
+public class P1 {
+    public static public static void main(String[] args) {
+        if(args[1]=="-d"){
+            //Realizar de fotma automatica una tabla que contenga el nombre del fichero, tipo, codificacion e idioma.
+        }else if(args[1]=="-l"){
+            // Recuperar todos los enlaces
+        }else if(args[1]=="-t"){
+
+        }else
+            System.out.println("Número de argumentos incorrectos. Argumentos: -d ; -l ; -t");
+    }
+    
+}
