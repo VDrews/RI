976b77065222d9b634ed4a6090fb6d0df15cdeae
VDrews <thedrews@outlook.es>
2021-10-01 21:01:54 +0200

    Parte -t: Hacer CSV completada

--- P1.java
+++ P1.java
@@ -1,12 +1,30 @@
 import org.apache.tika.sax.TeeContentHandler;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.apache.tika.Tika;
 import org.apache.tika.metadata.Metadata;
 import java.io.InputStream;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class P1 {
+  // Exporta un HashMap a CSV
+  public static void csvWriter(Map<String, Integer> hashmap, String pathname) throws IOException {
+    String eol = System.getProperty("line.separator");
+
+    try (Writer writer = new FileWriter(pathname + ".csv")) {
+      for (Map.Entry<String, Integer> entry : hashmap.entrySet()) {
+        writer.append(entry.getKey()).append(',').append(Integer.toString(entry.getValue())).append(eol);
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace(System.err);
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     Tika tika = new Tika();
     String[] pathnames;
@@ -36,10 +54,22 @@ public class P1 {
 
       // Recuperar todos los enlaces
     } else if (args[0].equals("-t")) {
+      for (String pathname : pathnames) {
+        File f = new File(pathname);
+
+        String text = tika.parseToString(f);
+        String[] parts = text.split(" ");
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String w : parts) {
+          Integer n = map.get(w);
+          n = (n == null) ? 1 : ++n;
+          map.put(w, n);
+        }
+        csvWriter(map, pathname);
+      }
 
     } else {
       System.out.println("Número de argumentos incorrectos. Argumentos: -d ; -l ; -t");
     }
   }
-
 }
