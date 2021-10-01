329e1998fce14cef0beee26d24ca2ab6b4725678
VDrews <thedrews@outlook.es>
2021-10-01 20:37:56 +0200

    Ahora recorrre los archivos con el primer parametro como directorio

--- P1.java
+++ P1.java
@@ -1,13 +1,39 @@
+import org.apache.tika.sax.TeeContentHandler;
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.tika.Tika;
+import org.apache.tika.metadata.Metadata;
+import java.io.InputStream;
 
 public class P1 {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    Tika tika = new Tika();
+    String[] pathnames;
+
+    File f_pathnames = new File(args[1]);
+
+    pathnames = f_pathnames.list();
+
     System.out.println(args[0]);
+
     if (args[0].equals("-d")) {
+      // Te dejo esto para que puedas recorrer el directorio,
+      // son las rutas, tienes que crear un file para cada uno
+      for (String pathname : pathnames) {
+        System.out.println(pathname);
+      }
 
       // Realizar de fotma automatica una tabla que contenga el nombre del fichero,
       // tipo, codificacion e idioma.
     } else if (args[0].equals("-l")) {
-      System.out.println("Parametro L");
+      for (String pathname : pathnames) {
+        System.out.println(pathname);
+      }
+      File f = new File(args[1]);
+
+      InputStream is = new FileInputStream(f);
+
       // Recuperar todos los enlaces
     } else if (args[0].equals("-t")) {
 
