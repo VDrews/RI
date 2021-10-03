f37f0fc1b8cf78591b01fb6d27c5e7db53835776
Adrian Carmona Lupiáñez <43786395+adriancarmona999@users.noreply.github.com>
2021-10-03 14:04:00 +0200

    Update P1.java

--- P1.java
+++ P1.java
@@ -1,11 +1,15 @@
 
+import org.apache.tika.Tika;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.metadata.Metadata;
+
 public class P1 {
   public static void main(String[] args) {
     System.out.println(args[0]);
     if (args[0].equals("-d")) {
-
       // Realizar de fotma automatica una tabla que contenga el nombre del fichero,
       // tipo, codificacion e idioma.
+      
     } else if (args[0].equals("-l")) {
       System.out.println("Parametro L");
       // Recuperar todos los enlaces
