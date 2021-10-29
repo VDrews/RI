672aafaf842d29ab1e7f3b08ea11a43e60a59aea
Adrian Carmona Lupiáñez <43786395+adriancarmona999@users.noreply.github.com>
2021-10-29 10:17:16 +0200

    P3 esquema hecho

--- P3/P3.java
+++ P3/P3.java
@@ -1,13 +1,44 @@
 package P3;
+import org.apache.lucene.analysis.core.*;
+
+import java.io.IOException;
+
+import com.mchange.net.SocketUtils;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.index.IndexWriter;
+
 
 public class P3 {
 
-
+private IndexWriter writer;
 
     public static void main(String[] args) {
         
     }
     
+    //Método para configurar el indice.
+    public void configurarIndice(Analyzer analyzer,Similarity Similarity){
+    
+    }
+    //Método para recoger la informacion de indexacion de los documentos, y añadirlos al indice.
+    public void indexarDocumentos(){
 
-    public void configurarIndice(Analyzer analyzer,)
+    }
+
+    //Método que maneja el cierre del indice.
+    public void close(){
+        try{
+            writer.commit();
+            writer.close();
+        }catch(IOException e){
+        System.out.println("¡Error cerrando el indice!")
+        }
+
+    }
+
+
+    
 }
